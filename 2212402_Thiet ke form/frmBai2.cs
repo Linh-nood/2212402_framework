613de4783eb2261cf6ee7e3fed2669e0fa0717c9
PC705 <PC705@PC705>
2024-08-20 10:53:49 +0700

    Add project files.

---
+++ 2212402_Thiet ke form/frmBai2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2212402_Thiet_ke_form
+{
+    public partial class frmBai2 : Form
+    {
+        public frmBai2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChonHang_Click(object sender, EventArgs e)
+        {
+            var imtem = listBox1.SelectedItem;
+            listBox2.Items.Add(imtem);
+        }
+    }
+}
---
+++ 2212402_framework/Mainform.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2212402_framework
+{
+    public partial class Mainform : Form
+    {
+        public Mainform()
+        {
+            InitializeComponent();
+        }
+        //click đôi vào chương trình sẽ tự động tạo sự kiện này
+       
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtSaoChep.Text = txtTen.Text;
+        }
+
+        private void btnOK_Click_1(object sender, EventArgs e)
+        {
+            var tenDaNhap = txtTen.Text;
+            MessageBox.Show($"Xin chào bạn {tenDaNhap}. Rất vui được ghặp bạn", "Thông điệp chào mừng");
+        }
+
+      
+
+        private void Mainform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTen_TextChanged(object sender, EventArgs e)
+        {
+            txtSaoChep.Text=txtTen.Text;
+        }
+    }
+}
